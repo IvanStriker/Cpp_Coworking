f0ae3d5c46f3103ebf5e5d48a6cf906f157f239c
Ivan Vinogradov <vinogradovivan548@gmail.com>
2025-11-23 18:33:52 +0300

    1-st meeting folder initialized

---
+++ 1-st meeting/code/unique.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+struct IntUnique {
+    IntUnique() = default;
+    IntUnique(const IntUnique &) = delete;
+    IntUnique &operator=(const IntUnique &) = delete;
+
+    IntUnique(IntUnique && other) noexcept : ptr_(other.ptr_) {
+        other.ptr_ = nullptr;
+    }
+
+    IntUnique& operator=(IntUnique && other) noexcept {
+        if (this == &other)
+            return *this;
+        delete ptr_;
+        ptr_ = other.ptr_;
+        other.ptr_ = nullptr;
+        return *this;
+    }
+
+    IntUnique(int* ptr) : ptr_(ptr) {}
+
+    ~IntUnique() {
+        delete ptr_;
+    }
+
+    int* get() {
+        return ptr_;
+    }
+
+    const int* get() const {
+        return ptr_;
+    }
+
+    void reset(int* ptr = nullptr) {
+        delete ptr_;
+        if (ptr)
+            ptr_ = ptr;
+    }
+
+    int& operator*() {
+        return *ptr_;
+    }
+
+    const int& operator*() const {
+        return *ptr_;
+    }
+
+private:
+    int* ptr_ = nullptr;
+};
